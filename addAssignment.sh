893171a6aec4b8ccf9165e8688586493b0bc112b
federico <fc@bcc.bz>
2023-03-17 19:39:21 +0000

    updated role assignment command

--- addAssignment.sh
+++ addAssignment.sh
@@ -3,7 +3,9 @@ set -ev
 if [ -z "$2" ]; then
 	echo "Pass projectId and UPN of test user"
 else
-	az role assignment create --assignee $2 --role 'Virtual Machine User Login' -g $1-RG --output none
+	rgId=$(az group show --name $1-RG --query id -o tsv)
+
+	az role assignment create --assignee $2 --role 'Virtual Machine User Login' --scope $rgId --output none
 	
 	agId=$(az desktopvirtualization applicationgroup show -g $1-RG -n $1-AG --query id -o tsv)
 	
